3b1dad938ab7ad7891a2e0686acc6c0fbfaf6fd2
Ram-eng01 <rammohan63025@gmail.com>
2026-02-23 17:04:11 +0530

    added python

---
+++ Day-3/1.1_calculator.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 20
+
+addition = num1 + num2
+print("Value of addition:",addition)
+
+sub = num1 - num2
+print("Value of sub:",sub)
+
+mul = num1 * num2
+print("Value of multiplication:", mul)
---
+++ Day-3/calcuator-new.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 67
+
+def addition():
+    add = num1 + num2
+    print("Value of addition:",add)
+
+def sub(num1,num2):
+    s = num1 - num2
+    return s
+
+def multiplication():
+    mul = num1 * num2
+    print("Value of multiplication:",mul)
+
+addition()
+print("vaue 0f sub:",sub(5,2))
+multiplication()
