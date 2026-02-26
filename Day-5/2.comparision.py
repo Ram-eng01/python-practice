9d9c8da8e4098aaca29136663f331a72e62a9c5c
Ram-eng01 <rammohan63025@gmail.com>
2026-02-26 14:19:05 +0530

    added python

---
+++ Day-5/2.comparision.py
@@ -0,0 +1,18 @@
+q = 10
+z = 34
+
+gretarthan = q > z
+lessthan = q < z
+gretarthanequals = q >= z
+lessthanequals = q <= z
+bothnotequal = q != z
+bothequal = q == z
+
+print("########################################")
+print("Comparision Operators:")
+print("q < z:", lessthan ) 
+print("q > z:",gretarthan)
+print("q <= z:",lessthanequals)
+print("q >= z:",gretarthanequals) 
+print("q != z:",bothnotequal)
+print("q == z:",bothequal)
