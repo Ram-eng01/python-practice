5642317d5dd56c89108c16451b1f97e1dcb395ff
Ram-eng01 <rammohan63025@gmail.com>
2026-02-26 14:12:22 +0530

    added python

---
+++ Day-5/1.Arthimatic.py
@@ -0,0 +1,21 @@
+a = 10
+b = 15
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+modulus = a % b
+float_division = a // b
+Exponential = a**b
+
+
+print("########################################")
+print("Arthimatic Operators:")
+print("Sum of A & B :",sum)
+print("Subtraction of A & B :",sub) 
+print("Multiplication of A & B :", mul) 
+print("Division of A & B :", div)
+print("Modulus of a and b", modulus)
+print("float devision", float_division)
+
