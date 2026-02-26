80d8ebbd646ecab626c69102c9b4370a1bda2ced
Ram-eng01 <rammohan63025@gmail.com>
2026-02-26 14:25:26 +0530

    added python

---
+++ Day-5/3.logical.py
@@ -0,0 +1,16 @@
+x = 67
+y = 78
+
+andlogic = x and y
+orlogic = x or y
+xnotlogic = not y
+ynotlogic = not x
+
+
+print("########################################")
+print("Logical Operators:") 
+print("x and y:", andlogic )
+print("x or y:",orlogic)
+print("x not y:",xnotlogic) 
+print("y not x:",ynotlogic)
+
