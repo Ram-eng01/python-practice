f5680ebb0aed33dad7ebb31a43fb1238606a8cfd
Ram-eng01 <rammohan63025@gmail.com>
2026-02-27 11:31:36 +0530

    added python

---
+++ Day-6/1.list.py
@@ -0,0 +1,15 @@
+my_list = [1,2,3,4,5,6,7,8,9, 678, 67, 45]
+
+print(my_list[0])
+
+
+my_list.append("rammohan")
+print(my_list)
+
+print(len(my_list))
+
+my_list.insert(2,"rajuuuuuuu") # insert rajuio at index 2
+print(my_list)
+
+print(my_list.pop())  # removes last element
+
---
+++ Day-6/2.tuple.py
@@ -0,0 +1,5 @@
+my_tuple = (1,3,5,"rahjkl",1,8,1)
+print(my_tuple.count(1))
+print(my_tuple.index(3))
+
+print(len(my_tuple))
